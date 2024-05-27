d975fdbb3063f3319e9108f78995989a31f2c22f
Will Hinson <wmhcell@gmail.com>
2024-05-26 21:08:16 -0400

    Incremented version to 0.2.0

--- sqlterm/__init__.py
+++ sqlterm/__init__.py
@@ -5,7 +5,7 @@ Contains the import of the SqlTerm class that is used in sqlterm.entrypoint to s
 a user session. Also contains definitions that indicate the current version of SqlTerm.
 """
 
-__version_info__: tuple[int, ...] = (0, 1, 0)
+__version_info__: tuple[int, ...] = (0, 2, 0)
 __version__: str = ".".join(map(str, __version_info__))
 
 from .sqlterm import SqlTerm
